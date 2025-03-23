4c2e2a1b0d7c74cdab6a230aadd7a73f2a38d281
SergYakov <akovlevsergej204@gmail.com>
2025-03-23 21:33:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,21 @@ app = Flask(__name__)
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def webhook():
-    data = request.get_json()
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        text = data["message"].get("text", "")
-
-        if text:
-            requests.post(API_URL, json={
-                "chat_id": chat_id,
-                "text": f"Шерлок получил: {text}"
-            })
-
-    return "ok"
-
-@app.route("/", methods=["GET"])
-def index():
-    return "Шерлок-бот работает."
+    if request.method == "POST":
+        data = request.get_json()
+        if "message" in data:
+            chat_id = data["message"]["chat"]["id"]
+            text = data["message"]["text"]
+            if text:
+                requests.post(API_URL, json={
+                    "chat_id": chat_id,
+                    "text": f"Шерлок получил: {text}"
+                })
+        return "ok"
+    else:
+        return "Шерлок-бот работает."
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
